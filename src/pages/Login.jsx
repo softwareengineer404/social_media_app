2cf290688f064e528ebd3611e0e180c21c3b335d
tesfay <tesfayhailemariam29@gmail.com>
2026-05-11 22:48:46 +0300

    Repeat Star component 5 times

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,6 +14,13 @@ const Login = () => {
         <div>
           <div className='flex items-center gap-3 mb-4 max-md:mt-10'>
             <img src={assets.group_users} alt="" className='h-8 md:h-10'/>
+            <div>
+              <div>
+                {Array(5).fill(0).map((_, i)=>(<Star key={i}
+                className='size-4 md:size-4.5 text-transparent
+                fill-amber-500'/>))}
+              </div>
+            </div>
           </div>
         </div>
       </div>
