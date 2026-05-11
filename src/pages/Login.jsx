1b07c5dfce6d11acd552c9b9977a2a3f49c04265
tesfay <tesfayhailemariam29@gmail.com>
2026-05-11 22:19:03 +0300

    add image logo

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,6 +6,14 @@ const Login = () => {
       {/* background Image */}
       <img src={assets.bgImage} alt="" className='absolute top-0 left-0 -z-1 w-full
       h-full object-cover'/>
+
+      {/* left side : Branding */}
+      <div className='flex-1 flex flex-col items-start justify-between p-6 md:p-10
+      lg:pl-40'>
+        <img src={assets.logo} alt="" className='h-12 object-contain'/>
+        
+      </div>
+
       
     </div>
   )
