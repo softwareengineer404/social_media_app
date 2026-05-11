f13238cb1be64c0015f3379963d579d3f6ecd9c1
tesfay <tesfayhailemariam29@gmail.com>
2026-05-11 22:12:21 +0300

    add background image

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      {/* background Image */}
+      <img src={assets.bgImage} alt="" className='absolute top-0 left-0 -z-1 w-full
+      h-full object-cover'/>
+      
+    </div>
   )
 }
 
