5d969be497092d90db8b5a67e199d54a222f28b9
tesfay <tesfayhailemariam29@gmail.com>
2026-05-12 09:15:27 +0300

    add h1 with its style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,6 +23,8 @@ const Login = () => {
               <p>Used by 12k+ developers</p>
             </div>
           </div>
+          <h1 className='text-3xl md:text-6xl md:pb-2 font-bold bg-gradient-to-r
+          from-indigo-950 to-indigo-800 bg-clip-text text-transparent'>More than friends truly connect</h1>
         </div>
       </div>
 
