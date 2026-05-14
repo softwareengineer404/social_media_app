4d7b6cf2365c6750640ef2e933d65f980fde1f4a
tesfay <tesfayhailemariam29@gmail.com>
2026-05-14 21:49:26 +0300

    adjust the if condition at the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,8 @@ import {BrowserRouter} from 'react-router-dom'
 import { ClerkProvider } from '@clerk/react'
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 if (!PUBLISHABLE_KEY) {
-  throw new Error() {
-    throw new Error('Missing publishable Key')
-  }
+  throw new Error("Something went wrong");
+
 }
 createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
