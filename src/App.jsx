4a67f0915c80bb382595e0ff03e06de7d38f33fb
tesfay <tesfayhailemariam29@gmail.com>
2026-05-14 21:53:06 +0300

    add the forget comma

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Login />}>
           <Route index element={<Feed/>}/>
-          <Route path='messages element={<Messages/>}/>
+          <Route path='messages' element={<Messages/>}/>
           <Route path='messages/:userId' element={<ChatBox/>}/>
           <Route path='connections' element={<Connections/>}/>
           <Route path='discover' element={<Discover/>}/>
