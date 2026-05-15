262a9fe1b6bc22eaaa3d85fd36e8e5f09ef13f71
tesfay <tesfayhailemariam29@gmail.com>
2026-05-15 22:43:37 +0300

    import layout

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,13 @@ import Discover from './pages/discover'
 import Profile from './pages/Profile'
 import CreatePost from './pages/CreatePost'
 import {useUser} from '@clerk/clerk-react'
+import Layout from './pages/Layout'
 const App = () => {
   const {user} = useUser()
   return (
     <>
       <Routes>
-        <Route path='/' element={<Login />}>
+        <Route path='/' element={ !user ? <Login /> : <Layout}>
           <Route index element={<Feed/>}/>
           <Route path='messages' element={<Messages/>}/>
           <Route path='messages/:userId' element={<ChatBox/>}/>
