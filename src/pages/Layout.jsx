346ed506bb5216f6b441e27bcd30e1e7bb15aa41
tesfay <tesfayhailemariam29@gmail.com>
2026-05-15 22:57:02 +0300

    import outlet and side bar in the layout page

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import Sidebar from '../components/Sidebar'
+import { Outlet } from 'react-router-dom'
 const Layout = () => {
   return (
-    <div>
+    <div className='w-full flex h-screen'>
+      <Sidebar />
+      <div className='flex-1 bg-slate-50'>
+        <Outlet />
+      </div>
       <h1>Layout</h1>
 
     </div>
