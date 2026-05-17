d5c8e602fbed934d958a77f71cd3c36c0dfd4410
tesfay <tesfayhailemariam29@gmail.com>
2026-05-17 22:29:42 +0300

    create side bar variable

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Sidebar = () => {
+const Sidebar = ({sidebarOpen, setSidebarOpen}) => {
   return (
-    <div>
-        Sidebar
+    <div className={`w-60 xl:w-72 bg-white border-r border-gray-200 flex flex-col
+    justify-between items-center max-sm:absolute top-0 bottom-0 z-20
+    ${sidebarOpen ? 'translate-x-0' : 'max-sm:-translate-x-full'}`}>
+        
     </div>
   )
 }
