fbd478098da9f2ddb51bbb96cb1371e609bd9045
tesfay <tesfayhailemariam29@gmail.com>
2026-05-19 09:00:10 +0300

    create page that contain posts

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import { dummyPostData } from '../assets/assets'
 const Feed = () => {
+  const [feeds, setfeeds] = useState([])
+  const fetchFeeds = async () => {
+    setfeeds(dummyPostData)
+  }
   return (
-    <div>Feed</div>
+    <div>
+      Feed
+    </div>
   )
 }
 
