c0c1f832cc63ba824e480751ce87b43d93e2ab60
tesfay <tesfayhailemariam29@gmail.com>
2026-05-19 09:56:15 +0300

    import useeffect and fetch stories

--- src/components/StoriesBar.jsx
+++ src/components/StoriesBar.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState}from 'react'
 import { dummyStoriesData } from '../assets/assets'
 const StoriesBar = () => {
     const [stories, setStories] = useState([])
     const fetchStories = async () => {
         setStories(dummyStoriesData)
     }
+    useEffect(()=>{
+        fetchStories()
+    },[])
   return (
     <div>
         StoriesBar
