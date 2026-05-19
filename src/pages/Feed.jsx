b064d4a9d973a556acdb1f55134e6d42c765986b
tesfay <tesfayhailemariam29@gmail.com>
2026-05-19 09:23:39 +0300

    create the feed page which contain the posted data

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Loading from '../components/Loading'
 import { dummyPostData } from '../assets/assets'
 const Feed = () => {
-  const [feeds, setfeeds] = useState([])
+  const [feeds, setfeeds] = useState([]) //feeds store post data,  setfeeds update the value of feeds
+  const [loading, setloading] = useState(true)
   const fetchFeeds = async () => {
     setfeeds(dummyPostData)
   }
-  return (
+  useEffect(()=>{
+    fetchFeeds()
+  },[])
+  return !loading ? (
     <div>
       Feed
     </div>
-  )
+  ) : <Loading />
 }
 
 export default Feed
