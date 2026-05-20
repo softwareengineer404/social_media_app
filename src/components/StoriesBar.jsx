7bc7f88c8fb342fa1fe52135432bc2de06497142
tesfay <tesfayhailemariam29@gmail.com>
2026-05-20 21:05:46 +0300

    add div class for create story

--- src/components/StoriesBar.jsx
+++ src/components/StoriesBar.jsx
@@ -11,6 +11,16 @@ const StoriesBar = () => {
   return (
     <div className='w-screen sm:w-[calc(100vw-240px)] lg:max-w-2xl no-scroolbar
     overflow-x-auto px-4'>
+        <div className='flex gap-4 pb-5'>
+            {/* add story card */}
+            <div className='rounded-lg shadow-sm min-w-30 max-h-40 aspect-
+            [3/4] cursor-pointer hover:shadow-lg transition-all duration-200
+            border-2 border-dashed border-indigo-300 bg-gradient-to-b
+            from-indigo-50 to-white'>
+
+            </div>
+            {/* story cards */}
+        </div>
         
     </div>
   )
