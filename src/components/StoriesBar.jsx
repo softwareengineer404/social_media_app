864b1c3977f697fc24dbf23f40a31733e5bc9f7a
tesfay <tesfayhailemariam29@gmail.com>
2026-05-20 21:52:26 +0300

    import + icon and the text Create Story

--- src/components/StoriesBar.jsx
+++ src/components/StoriesBar.jsx
@@ -21,8 +21,12 @@ const StoriesBar = () => {
                 p-4'>
                     <div className='size-10 bg-indigo-500 rounded-full flex
                     items-center justify-center mb-3'>
+                        <Plus className='w-5 h-5 text-white'/>
+
 
                     </div>
+                    <p className='text-sm font-medium text-slate-700
+                    text-center'>Create Story</p>
 
                 </div>
 
