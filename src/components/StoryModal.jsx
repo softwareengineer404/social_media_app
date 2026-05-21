956f55601a222a507d04bd2db5e78cb48e9c066f
tesfay <tesfayhailemariam29@gmail.com>
2026-05-21 20:28:09 +0300

    create new file under component the mode of story is text by default

---
+++ src/components/StoryModal.jsx
@@ -0,0 +1,12 @@
+import React, {useState }from 'react'
+
+const StoryModal = () => {
+    const [mode,setMode] = useState("text")
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default StoryModal
