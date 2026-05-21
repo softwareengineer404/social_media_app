55dba2d40b2c03e508c9cb8d4ed5191013e5d717
tesfay <tesfayhailemariam29@gmail.com>
2026-05-21 20:41:08 +0300

    create array color and set text the default story

--- src/components/StoryModal.jsx
+++ src/components/StoryModal.jsx
@@ -2,6 +2,12 @@ import React, {useState }from 'react'
 
 const StoryModal = () => {
     const [mode,setMode] = useState("text")
+    const bgColors = ["#4f46e5", "#7c3aed", "#db2777", "#e11d48", "#ca8a04", "#0d9488"]
+    const [mode, setMode] = useState("text")
+    const [background, setBackground] = useState(bgColors[0])
+    const [text, setText] = useState("")
+    const [media, setMedia] = useState("null")
+    const [previewUrl, setPreviewUrl] = useState("null")
   return (
     <div>
         
