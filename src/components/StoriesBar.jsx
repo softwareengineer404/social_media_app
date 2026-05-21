d47e81ec546d23f3c5284fb0e06fdf5988ab9ab3
tesfay <tesfayhailemariam29@gmail.com>
2026-05-21 20:21:11 +0300

    if the story is not text it dispaly image or video

--- src/components/StoriesBar.jsx
+++ src/components/StoriesBar.jsx
@@ -49,15 +49,23 @@ const StoriesBar = () => {
                         <p className='text-white absolute bottom-1 right-1 z-10
                         text-xs'>{moment(story.createdAt).fromNow()}</p>
                         {
-                            story.media_type === "image" ?
-                            <img src={story.media_url} alt="" className='h-full
-                            w-full object-cover hover:scale-110 transition
-                            duration-500 opacity-70 hover:opacity-80'/>
-                            :
-                            <video src={story.media_url} className='h-full w-full
-                            object-cover hover:scale-110 transition duration-500
-                            opacity-70 hover:opacity-80'/>
+                            story.media_type !== 'text' && (
+                                <div className='absolute inset-0 z-1 rounded-lg
+                                bg-black overflow-hidden'>
+                                      {
+                                        story.media_type === "image" ?
+                                        <img src={story.media_url} alt="" className='h-full
+                                        w-full object-cover hover:scale-110 transition
+                                        duration-500 opacity-70 hover:opacity-80'/>
+                                        :
+                                        <video src={story.media_url} className='h-full w-full
+                                        object-cover hover:scale-110 transition duration-500
+                                        opacity-70 hover:opacity-80'/>
+                                    }
+                                </div>
+                            )
                         }
+                       
 
                     </div>
                 ))
