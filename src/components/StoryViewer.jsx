132866e0d21dd416501e65a33506422a5bd57366
tesfay <tesfayhailemariam29@gmail.com>
2026-05-25 21:18:15 +0300

    add css for the progress bar

--- src/components/StoryViewer.jsx
+++ src/components/StoryViewer.jsx
@@ -6,6 +6,15 @@ const StoryViewer = ({viewStory, setViewStory}) => {
     items-center justify-center' style={{backgroundColor: viewStory.media_type ===
     'text' ? viewStory.background-color : '#000000'
     }}>
+        {/* progress bar */}
+        <div className='absolute top-0 left-0 w-full h-1 bg-gray-700'>
+            <div className='h-full bg-white transition-all duration-100 linear' style=
+            {{width: '50'}}>
+
+            </div>
+
+        </div>
+    
         
     </div>
   )
