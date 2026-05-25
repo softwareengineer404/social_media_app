3402217b821a6a8fc4e401648673047a6388efc2
tesfay <tesfayhailemariam29@gmail.com>
2026-05-25 21:36:50 +0300

    import Badge from lucide

--- src/components/StoryViewer.jsx
+++ src/components/StoryViewer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { BadgeCheck } from 'lucide-react'
 const StoryViewer = ({viewStory, setViewStory}) => {
   return (
     <div className='fixed inset-0 h-screen bg-black bg-opacity-90 z-110 flex
@@ -14,12 +14,15 @@ const StoryViewer = ({viewStory, setViewStory}) => {
             </div>
 
         </div>
+        {/* user info topleft*/}
         <div className='absolute top-4 left-4 flex items-center space-x-3 p-2 px-4
         sm:p-4 sm:px-8 backdrop-blur-2xl rounded bg-black/50'>
             <img src={viewStory.user?.profile_picture} alt="" className='ize-7
             sm:size-8 rounded-full object-cover border border-white'/>
-            <div>
+            <div className='text-white font-medium flex items-center gap-1.5'>
                 <span>{viewStory.user?.full_name}</span>
+                <BadgeCheck size={18}/>
+
             </div>
         </div>
     
