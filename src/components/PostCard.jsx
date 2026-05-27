7d51e214e1d14c1748f15805494240fdd9b1b088
tesfay <tesfayhailemariam29@gmail.com>
2026-05-27 09:52:40 +0300

    add css style and img source of profile picture

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const PostCard = ({post}) => {
   return (
-    <div>
-        
+    <div className='bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl'>
+        {/* user info */}
+        <div className='inline-flex items-center gap-3 cursor-pointer'>
+            <img src={post.user.profile_picture} alt="" className='w-10 h-10
+            rounded-full shadow'/>
+
+        </div>
     </div>
   )
 }
