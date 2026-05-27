a6e68e13d75db9109e4feb2ecb7c5ca8930083bf
tesfay <tesfayhailemariam29@gmail.com>
2026-05-27 09:31:35 +0300

    make it automatically close the story instead of giving manual or fixed percentage number

--- src/components/StoryViewer.jsx
+++ src/components/StoryViewer.jsx
@@ -58,7 +58,7 @@ const StoryViewer = ({viewStory, setViewStory}) => {
         {/* progress bar */}
         <div className='absolute top-0 left-0 w-full h-1 bg-gray-700'>
             <div className='h-full bg-white transition-all duration-100 linear' style=
-            {{width: '50%'}}>
+            {{width: `${progress}%`}}>
 
             </div>
 
