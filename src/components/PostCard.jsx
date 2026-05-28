1bfa9079aa158553f649f3d0d009c7896c1f0e36
tesfay <tesfayhailemariam29@gmail.com>
2026-05-28 19:40:33 +0300

    add photo or image to my code to be posted

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -19,9 +19,18 @@ const PostCard = ({post}) => {
             </div>
 
         </div>
-        {/* post content */}
-        {post.content && <div className='text-gray-800 text-sm
-        whitespace-pre-line' dangerouslySetInnerHTML={{__html: post.content}}/>}
+          {/* post content */}
+          {post.content && <div className='text-gray-800 text-sm
+          whitespace-pre-line' dangerouslySetInnerHTML={{__html: post.content}}/>}
+        {/* Images */}
+        <div className='grid grid-cols-2 gap-2'>
+            {post.image_urls.map((img, index)=>(
+                <img src={img} key={index} className={`w-full h-48 object-cover
+                rounded-lg ${post.image_urls.length === 1 && 'col-span-2 h-auto'}`}
+                alt="" />
+            ))}
+
+        </div>
     </div>
   )
 }
