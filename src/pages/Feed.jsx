614113475307b57d5217e5534b79adb78cd823b9
tesfay <tesfayhailemariam29@gmail.com>
2026-05-30 15:28:12 +0300

    add image source

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -30,7 +30,8 @@ const Feed = () => {
       <div className='max-xl:hidden sticky top-0'>
         <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex
         flex-col gap-2 shadow'>
-          <h1>Sponsored</h1>
+          <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
+          <img src={assets.sponsored_img} className='w-75 h-50 rounded-md' alt="" />
         </div>
         <h1>Recent messages</h1>
       </div>
