fc11f9f19115a45ad53fd322e350d4274958a680
tesfay <tesfayhailemariam29@gmail.com>
2026-05-31 13:04:33 +0300

    add image for the sender in the recent message and list recent message

--- src/components/RecentMessages.jsx
+++ src/components/RecentMessages.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { dummyRecentMessagesData } from '../assets/assets'
+import { Link } from 'react-router-dom'
 const RecentMessages = () => {
     const [messages, setMessages] = useState([])
     const fetchRecentMessages = async () => {
@@ -13,7 +14,18 @@ const RecentMessages = () => {
     <div className='bg-white max-w-xs mt-4 p-4 min-h-20 rounded-md shadow text-xs
     text-slate-800'>
         <h3 className='font-semibold text-slate-8 mb-4'>Recent Messages</h3>
-        
+        <div className='flex flex-col max-h-56 overflow-y-scroll no-scrollbar'>
+            {
+                messages.map((message, index)=>(
+                    <Link key={index} className='flex items-start gap-2 py-2
+                    hover:bg-slate-100'>
+                        <img src={message.from_user_id.profile_picture} alt=""
+                        className='w-8 h-8 rounded-full'/>
+                    </Link>
+                ))
+            }
+
+        </div>
     </div>
   )
 }
