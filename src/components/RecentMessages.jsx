a79035560a9fd4fa15ce662ce2226042f6bcfbe5
tesfay <tesfayhailemariam29@gmail.com>
2026-05-31 12:42:08 +0300

    create variable that can fetch recent messages

--- src/components/RecentMessages.jsx
+++ src/components/RecentMessages.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const RecentMessages = () => {
     const [messages, setMessages] = useState([])
+    const fetchRecentMessages = async () => {
+        setMessages(dummyRecentMessagesData)
+    }
   return (
     <div>
         
