109d99a16d08e356ba0ac65b50fad3abc1f5e2c9
tesfay <tesfayhailemariam29@gmail.com>
2026-06-01 13:52:08 +0300

    increase text-slate from 8 to 800

--- src/components/RecentMessages.jsx
+++ src/components/RecentMessages.jsx
@@ -13,7 +13,7 @@ const RecentMessages = () => {
   return (
     <div className='bg-white max-w-xs mt-4 p-4 min-h-20 rounded-md shadow text-xs
     text-slate-800'>
-        <h3 className='font-semibold text-slate-8 mb-4'>Recent Messages</h3>
+        <h3 className='font-semibold text-slate-800 mb-4'>Recent Messages</h3>
         <div className='flex flex-col max-h-56 overflow-y-scroll no-scrollbar'>
             {
                 messages.map((message, index)=>(
