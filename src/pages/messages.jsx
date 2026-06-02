0bb4bfb55ccf79651431686c09736382edc35758
tesfay <tesfayhailemariam29@gmail.com>
2026-06-02 17:34:48 +0300

    add tailwind css

--- src/pages/messages.jsx
+++ src/pages/messages.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const messages = () => {
   return (
-    <div>
-      <div>
+    <div className='min-h-screen relative bg-slate-50'>
+      <div className='max-w-6xl mx-auto p-6'>
         {/* tiltle */}
         <div>
           <h1>Messages</h1>
