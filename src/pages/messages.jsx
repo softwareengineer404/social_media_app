83d06b474dade05588775b154b750d04eda79b41
tesfay <tesfayhailemariam29@gmail.com>
2026-06-02 17:38:01 +0300

    add tailwind css for the h1 and p

--- src/pages/messages.jsx
+++ src/pages/messages.jsx
@@ -5,9 +5,9 @@ const messages = () => {
     <div className='min-h-screen relative bg-slate-50'>
       <div className='max-w-6xl mx-auto p-6'>
         {/* tiltle */}
-        <div>
-          <h1>Messages</h1>
-          <p>Talk to your friends and family</p>
+        <div className='mb-8'>
+          <h1 className='text-3xl font-bold text-slate-900 mb-2'>Messages</h1>
+          <p className='text-slate-600'>Talk to your friends and family</p>
           
         </div>
       </div>
