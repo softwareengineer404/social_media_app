a8d6776c02eaf087589df058037c4783375c7b79
tesfay <tesfayhailemariam29@gmail.com>
2026-06-03 08:20:42 +0300

    make it onclick for the messagesquare icon

--- src/pages/messages.jsx
+++ src/pages/messages.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import { Eye, MessageSquare }from 'lucide-react'
+import { dummyConnectionsData } from '../assets/assets'
+import { Eye, MessageSquare } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 const messages = () => {
+  const navigate = useNavigate()
   return (
     <div className='min-h-screen relative bg-slate-50'>
       <div className='max-w-6xl mx-auto p-6'>
@@ -31,7 +34,7 @@ const messages = () => {
                 <p className='text-sm text-gray-600'>{user.bio}</p>
               </div>
               <div>
-                <button className='size-10 flex items-center justify-center text-sm
+                <button onClick={() => navigate(`/messages/${user._id}`)} className='size-10 flex items-center justify-center text-sm
                 rounded bg-slate-100 hover:bg-slate-200 text-slate-800
                 active:scale-95 transition cursor-pointer gap-1'>
                   <MessageSquare className="w-4 h-4"/>
