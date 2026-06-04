1c27678ad02c45b7276ba2492f5f75609f8b849b
tesfay <tesfayhailemariam29@gmail.com>
2026-06-04 16:19:55 +0300

    import the icons like Users, UserPlus, UserCheck, UserRoundPen, MessageSquare

--- src/pages/connections.jsx
+++ src/pages/connections.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import {Users, UserPlus, UserCheck, UserRoundPen, MessageSquare} from 'lucide-react'
+import { useNavigate } from 'react-router-dom';
 const connections = () => {
   return (
-    <div>connections</div>
+    <div>
+      
+    </div>
   )
 }
 
