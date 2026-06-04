6bf81ea5ca5a0ea71f21e88cef007b0c187f90e0
tesfay <tesfayhailemariam29@gmail.com>
2026-06-04 16:26:42 +0300

    import the icons like follow , following

--- src/pages/connections.jsx
+++ src/pages/connections.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import {Users, UserPlus, UserCheck, UserRoundPen, MessageSquare} from 'lucide-react'
 import { useNavigate } from 'react-router-dom';
+import {
+  dummyConnectionsData as connections,
+  dummyFollowersData as followers,
+  dummyFollowingData as following,
+  dummyPendingConnectionsData as pendingConnections
+} from '../assets/assets'
 const connections = () => {
   return (
     <div>
