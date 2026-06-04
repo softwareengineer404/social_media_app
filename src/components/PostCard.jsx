dbcbd9275bde188ee498ac72b3c8963598f3ffaa
tesfay <tesfayhailemariam29@gmail.com>
2026-06-04 16:07:22 +0300

    make it onclick for the profile

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -14,7 +14,7 @@ const PostCard = ({post}) => {
   return (
     <div className='bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl'>
         {/* user info */}
-        <div className='inline-flex items-center gap-3 cursor-pointer'>
+        <div onClick={()=> navigate('/profile/' +post.user._id)}className='inline-flex items-center gap-3 cursor-pointer'>
             <img src={post.user.profile_picture} alt="" className='w-10 h-10
             rounded-full shadow'/>
             <div>
