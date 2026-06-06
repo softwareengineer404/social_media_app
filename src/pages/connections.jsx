c5f8ce3f96bc31c1e24ebe0a36a7595c4aacdd80
tesfay <tesfayhailemariam29@gmail.com>
2026-06-06 08:30:22 +0300

    If the current tab is Connections, show a Message button with a message icon.
    When clicked, it should navigate to a messaging page.

--- src/pages/connections.jsx
+++ src/pages/connections.jsx
@@ -101,6 +101,16 @@ const connections = () => {
                       </button>
                     )
                   }
+                  {
+                    currentTab === 'Connections' && (
+                      <button onClick={()=> navigate(`/messages/${}`)}className='w-full p-2 text-sm rounded bg-slate-100
+                      hover:bg-slate-200 text-slate-800 active:scale-95 transition
+                      cursor-pointer flex items-center justify-center gap-1'>
+                        <MessageSquare className='w-4 h-4'/>
+                        Message
+                      </button>
+                    )
+                  }
                 </div>
               </div>
           </div>
