98e5674efac0ecb315f91b41dadee427f893529a
tesfay <tesfayhailemariam29@gmail.com>
2026-06-07 16:43:39 +0300

    initialize useState and other variables for search bar(input value) and list of users

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { dummyConnectionsData } from '../assets/assets'
 const Discover = () => {
+  const [input, setInput] = useState('')
+  const [users, setUsers] = useState(dummyConnectionsData)
+  const [loading, setLoading] = useState(false)
   return (
-    <div>Discover</div>
+    <div>
+      
+    </div>
   )
 }
 
