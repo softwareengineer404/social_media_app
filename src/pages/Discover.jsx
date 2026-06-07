15e3df35f3791d9f4ada4f62e34c1ba4999bee5d
tesfay <tesfayhailemariam29@gmail.com>
2026-06-07 16:51:25 +0300

    create async function

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -4,6 +4,17 @@ const Discover = () => {
   const [input, setInput] = useState('')
   const [users, setUsers] = useState(dummyConnectionsData)
   const [loading, setLoading] = useState(false)
+
+  const handleSearch = async (e) => {
+    if(e.key === 'enter'){
+      setUsers([])
+      setLoading(true)
+      setTimeout(()=>{
+        setUsers(dummyConnectionsData)
+        setLoading(false)
+      }, 1000)
+    }
+  }
   return (
     <div>
       
