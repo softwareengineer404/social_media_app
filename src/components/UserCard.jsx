453d07e7ad74b4bd9d7a506475889a2658636f89
tesfay <tesfayhailemariam29@gmail.com>
2026-06-08 09:18:20 +0300

    create new file usercards and use the dummy data from the assets

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { dummyUserData } from '../assets/assets'
+const UserCard = () => {
+    const currentUser = dummyUserData
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default UserCard
