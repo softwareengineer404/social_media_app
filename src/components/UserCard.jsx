18de43bb3e1e62564fd30beb285a34e9230af832
tesfay <tesfayhailemariam29@gmail.com>
2026-06-08 09:34:00 +0300

    add profile_picture

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { dummyUserData } from '../assets/assets'
-const UserCard = () => {
+const UserCard = ({user}) => {
     const currentUser = dummyUserData
     const handleFollow = async () => {
 
     }
     const handleConnectionRequest = async () => {
-        
+
     }
   return (
-    <div>
-        
+    <div key={user._id}className='p-4 pt-6 flex flex-col justify-between w-72 shadow border
+    border-gray-200 rounded-md'>
+        <div>
+            <img src={user.profile_picture} alt="" className='rounded-full w-16
+            shadow-md mx-auto'/>
+        </div>
     </div>
   )
 }
