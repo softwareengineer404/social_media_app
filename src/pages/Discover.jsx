4451d33059c6a93154b8cfe04c876e16bda93f88
tesfay <tesfayhailemariam29@gmail.com>
2026-06-08 09:13:08 +0300

    This component creates a search bar with a search icon, stores what the user types in React state (input), and triggers handleSearch whenever a key is released

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -28,7 +28,12 @@ const Discover = () => {
       80'>
         <div className='p-6'>
           <div className='relative'>
-            
+            <Search className='absolute left-3 top-1/2 transform -translate-y-1/2
+            text-slate-400 w-5 h-5' />
+            <input type="text" placeholder='Search people by name, username, bio,
+            or location...' className='pl-10 sm:pl-12 py-2 w-full border
+            border-gray-300 rounded-md max-sm:text-sm' onChange={(e)=>setInput(e.
+            target.value)} value={input} onKeyUp={handleSearch}/>
 
           </div>
         </div>
