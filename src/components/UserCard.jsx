7eb7f8be858e389bd4aa30c593b3f339bf6d6796
tesfay <tesfayhailemariam29@gmail.com>
2026-06-08 09:41:12 +0300

    add bio to the page

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -11,11 +11,12 @@ const UserCard = ({user}) => {
   return (
     <div key={user._id}className='p-4 pt-6 flex flex-col justify-between w-72 shadow border
     border-gray-200 rounded-md'>
-        <div>
+        <div className='text-center'>
             <img src={user.profile_picture} alt="" className='rounded-full w-16
             shadow-md mx-auto'/>
             <p className='mt-4 font-semibold'>{user.full_name}</p>
             {user.username && <p className='text-gray-500 font-light'>@{user.username}</p>}
+            {user.bio && <p className='text-gray-600 mt-2 text-center text-sm px-4'>{user.bio}</p>}
         </div>
     </div>
   )
