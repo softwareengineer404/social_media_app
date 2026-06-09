f6a81ead3eda2658001a41ce9a8f2d3e9ea6be9f
tesfay <tesfayhailemariam29@gmail.com>
2026-06-09 14:44:59 +0300

    add tailwind css for the parent div

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -18,7 +18,8 @@ const UserCard = ({user}) => {
             {user.username && <p className='text-gray-500 font-light'>@{user.username}</p>}
             {user.bio && <p className='text-gray-600 mt-2 text-center text-sm px-4'>{user.bio}</p>}
         </div>
-        <div>
+        <div className='flex items-center justify-center gap-2 mt-4 text-xs
+        text-gray-600'>
             <div className='flex items-center gap-1 border border-gray-300
             rounded-full px-3 py-1'>
                 <MapPin className='w-4 h-4'/> (user.location)
