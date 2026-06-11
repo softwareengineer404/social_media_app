cc4a7ab3aa864d4b37b1d0f67c045dfb4c0b8e8a
tesfay <tesfayhailemariam29@gmail.com>
2026-06-11 15:03:10 +0300

    make it disable the button if alread follow the user

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -34,7 +34,8 @@ const UserCard = ({user}) => {
         </div>
         <div className='flex mt-4 gap-2'>
             {/* Follow Button */}
-            <button className='w-full py-2 rounded-md flex justify-center
+            <button onClick={handleFollow} disabled={currentUser?.following.includes
+            (user._id)} className='w-full py-2 rounded-md flex justify-center
             items-center gap-2 bg-gradient-to-r from-indigo-500 to-purple-600
             hover:from-indigo-600 hover:to-purple-700 active:scale-95 transition
             text-white cursor-pointer'>
