771ccf7e1e709f32622e023f71f008b66be8f184
tesfay <tesfayhailemariam29@gmail.com>
2026-06-12 16:21:43 +0300

    create function that fetch user data

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { dummyPostsData, dummyUserData } from '../assets/assets'
+import { useEffect } from 'react'
 const profile = () => {
   const {profile} = useParams()
   const [user, setUser] = useState(null)
   const [posts, setPosts] = useState([])
   const [activeTab, setActiveTab] = useState('posts')
   const [showEdit, setShowEdit] = useState('false')
+  const fetchUser = async () => {
+    setUser(dummyUserData)
+    setPosts(dummyPostsData)
+  }
   return (
     <div>
       
