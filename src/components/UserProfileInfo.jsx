1e08578a4ee6c7745affd87d2adc3c41c3553f44
tesfay <tesfayhailemariam29@gmail.com>
2026-06-13 14:55:37 +0300

    add tailwind css for the div

--- src/components/UserProfileInfo.jsx
+++ src/components/UserProfileInfo.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const UserProfileInfo = ({user, posts, profileId,setShowEdit}) => {
   return (
-    <div>
-        <div>
+    <div className='relative py-4 px-6 md:8 bg-white'>
+        <div className='flex flex-col md:flex-row items-start gap-6'>
             <div>
                 <img src={user.profile_picture} alt="" className='absolute rounded-full
                 z-2'/>
