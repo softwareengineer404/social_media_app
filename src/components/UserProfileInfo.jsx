26bd04ecfcecd18ebe8f7ddbe798350b759fee06
tesfay <tesfayhailemariam29@gmail.com>
2026-06-13 14:53:17 +0300

    create image src

--- src/components/UserProfileInfo.jsx
+++ src/components/UserProfileInfo.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const UserProfileInfo = ({user, posts, profileId,setShowEdit}) => {
   return (
     <div>
-        
+        <div>
+            <div>
+                <img src={user.profile_picture} alt="" className='absolute rounded-full
+                z-2'/>
+            </div>
+        </div>
     </div>
   )
 }
