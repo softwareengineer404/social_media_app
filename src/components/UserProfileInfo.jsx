3612b74516c131989705b4b37a9f651334f6425e
tesfay <tesfayhailemariam29@gmail.com>
2026-06-13 14:57:43 +0300

    add another css

--- src/components/UserProfileInfo.jsx
+++ src/components/UserProfileInfo.jsx
@@ -4,7 +4,8 @@ const UserProfileInfo = ({user, posts, profileId,setShowEdit}) => {
   return (
     <div className='relative py-4 px-6 md:8 bg-white'>
         <div className='flex flex-col md:flex-row items-start gap-6'>
-            <div>
+            <div className='w-32 h-32 border-4 border-white shadow-lg absolute -top-16
+            rounded-full'>
                 <img src={user.profile_picture} alt="" className='absolute rounded-full
                 z-2'/>
             </div>
