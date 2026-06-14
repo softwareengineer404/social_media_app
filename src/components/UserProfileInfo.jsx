4cf7cd5c5244267586cc8effa1d79ccd8f85e348
tesfay <tesfayhailemariam29@gmail.com>
2026-06-14 15:55:58 +0300

    import verified and get full_name in the h1 tag

--- src/components/UserProfileInfo.jsx
+++ src/components/UserProfileInfo.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Verified } from 'lucide-react'
 const UserProfileInfo = ({user, posts, profileId,setShowEdit}) => {
   return (
     <div className='relative py-4 px-6 md:8 bg-white'>
@@ -8,6 +8,16 @@ const UserProfileInfo = ({user, posts, profileId,setShowEdit}) => {
             rounded-full'>
                 <img src={user.profile_picture} alt="" className='absolute rounded-full
                 z-2'/>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            <h1 className='text-2xl font-bold text-gray-900'>{user.full_name}</h1>
+                            <Verified className='w-6 h-6 text-blue-500'/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
