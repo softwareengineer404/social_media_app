e746ef920f9ff4910c8f37518b82b1c5222b6acd
tesfay <tesfayhailemariam29@gmail.com>
2026-06-15 17:49:29 +0300

    add cursor pointer

--- src/components/UserProfileInfo.jsx
+++ src/components/UserProfileInfo.jsx
@@ -24,7 +24,8 @@ const UserProfileInfo = ({user, posts, profileId,setShowEdit}) => {
                     {!profileId &&
                         <button onClick={()=> setShowEdit(true)} className='flex
                         items-center gap-2 border border-gray-300 hover:bg-gray-50 px-4
-                        py-2 rounded-lg font-medium transition-colors mt-4 md:mt-0'>
+                        py-2 rounded-lg font-medium transition-colors mt-4 md:mt-0
+                        cursor-pointer'>
                             <PenBox className='w-4 h-4'/>
                             Edit
                         </button>}
