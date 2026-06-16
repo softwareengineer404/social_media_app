e85e0a48d8fab3fdef0a623619d89a88e44b3de1
tesfay <tesfayhailemariam29@gmail.com>
2026-06-16 17:31:21 +0300

    display number of posts

--- src/components/UserProfileInfo.jsx
+++ src/components/UserProfileInfo.jsx
@@ -42,6 +42,12 @@ const UserProfileInfo = ({user, posts, profileId,setShowEdit}) => {
                         fromNow()}</span>
                     </span>
                 </div>
+                <div className='flex items-center gap-6 mt-6 border-t border-gray-200
+                pt-4'>
+                    <div>
+                        <span className='sm:text-xl font-bold text-gray-900'>{posts.length}</span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
