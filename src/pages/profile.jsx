7c01df4e0bc11cccff4b3d7295275fdcc25d645d
tesfay <tesfayhailemariam29@gmail.com>
2026-06-17 16:52:08 +0300

    It shows the user's posts only when the Posts tab is selected

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -45,6 +45,12 @@ const profile = () => {
               </button>
             ))}
           </div>
+          {/* Posts */}
+          {activeTab === 'posts' && (
+            <div className='mt-6 flex flex-col items-center gap-6'>
+              {posts.map((post)=> <PostCard key={post._id} post={post}/>)}
+            </div>
+          )}
         </div>
       </div>
     </div>
