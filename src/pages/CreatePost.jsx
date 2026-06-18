f5ec5e6205d92da5a17e5207c4157666c33e3a44
tesfay <tesfayhailemariam29@gmail.com>
2026-06-18 17:57:58 +0300

    add full name and user name at the side of profile picture

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -13,6 +13,16 @@ const CreatePost = () => {
           <h1 className='text-3xl font-bold text-slate-900 mb-2'>Create Post</h1>
           <p className='text-slate-600'>Share your thoughts with the world</p>
         </div>
+        <div>
+          {/* Header */}
+          <div>
+            <img/>
+            <div>
+              <h2 className='font-semibold'>{user.full_name}</h2>
+              <p className='text-sm text-gray-500'>@{user.username}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
