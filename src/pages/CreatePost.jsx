48c696c9a3884d64351142aee5f943b6bdb52ead
tesfay <tesfayhailemariam29@gmail.com>
2026-06-18 17:45:49 +0300

    add title and description

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -4,9 +4,16 @@ const CreatePost = () => {
   const [content, setContent] = useState('')
   const [images, setImages] = useState([])
   const [loading, setLoading] = useState(false)
+  const user = dummyUserData;
   return (
     <div>
-      CreatePost
+      <div>
+        {/* title */}
+        <div>
+          <h1 className='text-3xl font-bold text-slate-900 mb-2'>Create Post</h1>
+          <p className='text-slate-600'>Share your thoughts with the world</p>
+        </div>
+      </div>
     </div>
   )
 }
