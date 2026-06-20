9bccf95ee7b0b87447f6b8aac62313cef28d5c8e
tesfay <tesfayhailemariam29@gmail.com>
2026-06-20 16:30:17 +0300

    give tailwind css for the bottom bar container

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -48,7 +48,8 @@ const CreatePost = () => {
             <div/>
           }
             {/* Bottom Bar */} 
-            <div>
+            <div className='flex items-center justify-between pt-3 border-t
+            border-gray-300'>
               <label htmlFor="images" className='flex items-center gap-2 text-sm
               text-gray-500 hover:text-gray-700 transition cursor-pointer'>
                 <Image className='size-6'/>
