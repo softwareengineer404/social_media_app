c7341c2c55ad3c8a4748f4303ac37e990be932e2
tesfay <tesfayhailemariam29@gmail.com>
2026-06-21 17:29:39 +0300

    add profile _picture when the user chat

--- src/pages/ChatBox.jsx
+++ src/pages/ChatBox.jsx
@@ -13,9 +13,14 @@ const ChatBox = () => {
   useEffect(()=>{
     messagesEndRef.current?.scrollIntoView({behavior: "smooth" })
   },[messages])
-  return (
-    <div>
-      
+  return user && (
+    <div className='flex flex-col h-screen'>
+      <div className='flex items-center gap-2 p-2 md:px-10 xl:pl-42
+      bg-gradient-to-r from-indigo-50 to-purple-50 border-b border-gray-300'>
+
+        <img src={user.profile_picture} alt="" className="size-8 rounded-full"/>
+        <div></div>
+      </div>
     </div>
   )
 }
