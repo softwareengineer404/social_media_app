a74d167927d1ad591c11b7a5eac0a24df3ef7ec2
tesfay <tesfayhailemariam29@gmail.com>
2026-06-21 17:14:30 +0300

    create some functions that are used to chatting

--- src/pages/ChatBox.jsx
+++ src/pages/ChatBox.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { dummyMessagesData, dummyUserData } from '../assets/assets'
 
 const ChatBox = () => {
+  const messages = dummyMessagesData
+  const [text, setText] = useState('')
+  const [image, setImage] = useState(null)
+  const [user, setUser] = useState(dummyUserData)
+  const messagesEndRef = useRef(null)
   return (
-    <div>ChatBox</div>
+    <div>
+      
+    </div>
   )
 }
 
