ca3f9438e3768ff1ac32f885f73a88bb21d8db4b
tesfay <tesfayhailemariam29@gmail.com>
2026-06-22 16:17:06 +0300

    add image source to be send

--- src/pages/ChatBox.jsx
+++ src/pages/ChatBox.jsx
@@ -32,7 +32,13 @@ const ChatBox = () => {
               <div key={index} className={`flex flex-col ${message.to_user_id !== 
               user._id ? 'items-start' : 'items-end'}`}>
               
-                <div></div>
+                <div>
+                  {
+                  message.message_type === 'image' && <img src={message.media_url}
+                  className='w-full max-w-sm rounded-lg mb-1' alt="" />
+                  }
+                  <p>{message.text}</p>
+                </div>
               </div>
             ))
             
