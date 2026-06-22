efdc373285c92fa331b0ca2a238ff76fae7fd65c
tesfay <tesfayhailemariam29@gmail.com>
2026-06-22 16:24:00 +0300

    add tailwind css for the div container

--- src/pages/ChatBox.jsx
+++ src/pages/ChatBox.jsx
@@ -32,7 +32,10 @@ const ChatBox = () => {
               <div key={index} className={`flex flex-col ${message.to_user_id !== 
               user._id ? 'items-start' : 'items-end'}`}>
               
-                <div>
+                <div className={`p-2 text-sm max-w-sm bg-white text-slate-700
+                rounded-lg shadow ${message.to_user_id !== user._id ?
+                'rounded-bl-none' : 'rounded-br-none'}`}>
+              
                   {
                   message.message_type === 'image' && <img src={message.media_url}
                   className='w-full max-w-sm rounded-lg mb-1' alt="" />
