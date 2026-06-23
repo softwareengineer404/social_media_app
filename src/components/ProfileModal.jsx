eadcc42a6a1afac12991d4ae8d65dce730f52acc
tesfay <tesfayhailemariam29@gmail.com>
2026-06-23 17:00:32 +0300

    add title and give it the tailwind css for the container

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -9,8 +9,17 @@ const ProfileModal = () => {
         profile_picture: null,
         full_name: user.full_name,
     })
+    const handleSaveProfile = async (e) => {
+        e.preventDefault();
+    }
   return (
-    <div>
+    <div className='fixed top-0 bottom-0 left-0 right-0 z-110 h-screen
+    overflow-y-scroll bg-black/50'>
+        <div className='max-w-2xl sm:py-6 mx-auto'>
+            <div className='bg-white rounded-lg shadow p-6'>
+                <h1 className='text-2xl font-bold text-gray-900 mb-6'>Edit Profile</h1>
+            </div>
+        </div>
 
     </div>
   )
