f94355e17e99544de306690fbae9505d3bb4d33e
tesfay <tesfayhailemariam29@gmail.com>
2026-06-23 16:50:35 +0300

    create a variable that can store the form to be edit

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { dummyUserData } from '../assets/assets'
 const ProfileModal = () => {
+    const user = dummyUserData;
+    const [editForm, setEditForm] = useState({
+        username: user.username,
+        bio: user.bio,
+        location: user.location,
+        profile_picture: null,
+        full_name: user.full_name,
+    })
   return (
     <div>
-        
+
     </div>
   )
 }
