f8844c33dc55783b8d8731685be7398fdb17aef4
tesfay <tesfayhailemariam29@gmail.com>
2026-06-26 18:06:20 +0300

    add input box that we can update the name of profile

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -63,7 +63,17 @@ const ProfileModal = () => {
                             
                         </div>
                     </label>
-                </div>  
+                </div> 
+                <div>
+                    <label className="block text-sm font-medium text-gray-700 mb-1">
+                        Name
+                    </label>
+                        <input type="text" className='w-full p-3 border border-gray-200
+                        rounded-lg' placeholder='please enter your full name' onChange={(e)
+                        =>setEditForm({...editForm, full_name: e.target.value})} value={editForm.full_name}/>
+                    
+                    
+                </div>
                 </form>
             </div>
         </div>
