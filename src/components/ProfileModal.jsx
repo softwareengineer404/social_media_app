851272c44afe8da8aadf90eeaabf32931193d98e
tesfay <tesfayhailemariam29@gmail.com>
2026-06-26 18:13:49 +0300

    create a space to edit a bio

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -84,6 +84,16 @@ const ProfileModal = () => {
                     
                     
                 </div>
+                <div>
+                    <label className="block text-sm font-medium text-gray-700 mb-1">
+                        Bio
+                    </label>
+                        <textarea rows={3} className='w-full p-3 border border-gray-200
+                        rounded-lg' placeholder='please enter your bio' onChange={(e)
+                        =>setEditForm({...editForm, bio: e.target.value})} value={editForm.bio}/>
+                    
+                    
+                </div>
                 </form>
             </div>
         </div>
