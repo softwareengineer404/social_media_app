c4baa76ede94e5dec6d06ba3f1c545cd13cfb20d
tesfay <tesfayhailemariam29@gmail.com>
2026-06-27 17:51:05 +0300

    add cancel button in the edit profile

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { dummyUserData } from '../assets/assets'
 import { Pencil } from 'lucide-react';
-const ProfileModal = () => {
+const ProfileModal = ({setShowEdit}) => {
     const user = dummyUserData;
     const [editForm, setEditForm] = useState({
         username: user.username,
@@ -103,6 +103,12 @@ const ProfileModal = () => {
                         =>setEditForm({...editForm, location: e.target.value})} value={editForm.location}/>
                     
                     
+                </div>
+                <div className='flex justify-end space-x-3 pt-6'>
+                    <button onClick={()=> setShowEdit(false)} type='button
+                    className='px-4 py-2 border border-gray-300 rounded-lg
+                    text-gray-700 hover:bg-gray-50 transition-colors'>Cancel</button>
+                    <button></button>
                 </div>
                 </form>
             </div>
