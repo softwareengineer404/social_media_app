748601c2c0f40f1b8ef6d02219fef6792f4bbd5c
tesfay <tesfayhailemariam29@gmail.com>
2026-06-27 17:55:54 +0300

    add another button save changes

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -108,7 +108,10 @@ const ProfileModal = ({setShowEdit}) => {
                     <button onClick={()=> setShowEdit(false)} type='button
                     className='px-4 py-2 border border-gray-300 rounded-lg
                     text-gray-700 hover:bg-gray-50 transition-colors'>Cancel</button>
-                    <button></button>
+                    <button type='submit' className='px-4 py-2 bg-gradient-to-r
+                    from-indigo-500 to-purple-600 text-white rounded-lg
+                    hover:from-indigo-600 hover:to-purple-700 transition
+                    cursor-pointer'>Save Changes</button>
                 </div>
                 </form>
             </div>
