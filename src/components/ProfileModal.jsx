c4dcc8fb949b33460ff7bac7f2601899ed63f2a0
tesfay <tesfayhailemariam29@gmail.com>
2026-06-30 17:19:55 +0300

    avoid extra space b/n the divs

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -89,7 +89,6 @@ const ProfileModal = ({setShowEdit}) => {
                         <textarea rows={3} className='w-full p-3 border border-gray-200
                         rounded-lg' placeholder='please enter your bio' onChange={(e)
                         =>setEditForm({...editForm, bio: e.target.value})} value={editForm.bio}/>
-                    
                 </div>
                 <div>
                     <label className="block text-sm font-medium text-gray-700 mb-1">
