c38f9a20bd273b5859185b459228622502786588
tesfay <tesfayhailemariam29@gmail.com>
2026-07-09 18:16:10 +0300

    check the login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import { assets } from '../assets/assets'
 import { Star } from 'lucide-react'
 import {SignIn } from '@clerk/clerk-react'
-
 const Login = () => {
   return (
     <div className='min-h-screen flex flex-col md:flex-row'>
       {/* background Image */}
       <img src={assets.bgImage} alt="" className='absolute top-0 left-0 -z-1 w-full
       h-full object-cover'/>
-
       {/* left side : Branding */}
       <div className='flex-1 flex flex-col items-start justify-between p-6 md:p-10
       lg:pl-40'>
@@ -41,5 +39,4 @@ const Login = () => {
     </div>
   )
 }
-
 export default Login
