79d37d8aea9a11f6ee1e4844ee1df357036fe28a
tesfay <tesfayhailemariam29@gmail.com>
2026-07-10 17:14:56 +0300

    capitalize the paragraph text

--- src/pages/messages.jsx
+++ src/pages/messages.jsx
@@ -10,7 +10,7 @@ const messages = () => {
         {/* tiltle */}
         <div className='mb-8'>
           <h1 className='text-3xl font-bold text-slate-900 mb-2'>Messages</h1>
-          <p className='text-slate-600'>Talk to your friends and family</p>
+          <p className='text-slate-600'>Talk To Your Friends and Family</p>
         </div>
         {/* connected users*/}
         <div className='flex flex-col gap-3'>
