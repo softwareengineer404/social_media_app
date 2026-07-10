5aec80045a771273d811275f00b240239fc127bf
tesfay <tesfayhailemariam29@gmail.com>
2026-07-10 17:13:59 +0300

    fix the error inside the message.jsx file

--- src/pages/messages.jsx
+++ src/pages/messages.jsx
@@ -11,7 +11,6 @@ const messages = () => {
         <div className='mb-8'>
           <h1 className='text-3xl font-bold text-slate-900 mb-2'>Messages</h1>
           <p className='text-slate-600'>Talk to your friends and family</p>
-        
         </div>
         {/* connected users*/}
         <div className='flex flex-col gap-3'>
