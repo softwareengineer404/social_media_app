5c4535d2fab6a6f79c2de33f8e1a5e86269dd73d
tesfay <tesfayhailemariam29@gmail.com>
2026-07-10 17:11:57 +0300

    checking of error in the message.jsx

--- src/pages/messages.jsx
+++ src/pages/messages.jsx
@@ -12,7 +12,6 @@ const messages = () => {
           <h1 className='text-3xl font-bold text-slate-900 mb-2'>Messages</h1>
           <p className='text-slate-600'>Talk to your friends and family</p>
         
-          
         </div>
         {/* connected users*/}
         <div className='flex flex-col gap-3'>
