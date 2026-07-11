b6dec6093715a786e8a9da2980b7d619ed2baef2
tesfay <tesfayhailemariam29@gmail.com>
2026-07-11 18:43:04 +0300

    remove the space

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,6 @@ const App = () => {
           <Route path='profile' element={<Profile/>}/>
           <Route path='profile/:profileId' element={<Profile/>}/>
           <Route path='create-post' element={<CreatePost/>}/>
-
         </Route>
       </Routes>
       App
