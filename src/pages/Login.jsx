47bbfe24bee7fa45ecc5cba8e69394473ec6e7bb
tesfay <tesfayhailemariam29@gmail.com>
2026-07-16 16:56:20 +0300

    capitalize the comment

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
       {/* background Image */}
       <img src={assets.bgImage} alt="" className='absolute top-0 left-0 -z-1 w-full
       h-full object-cover'/>
-      {/* left side : Branding */}
+      {/* Left Side : Branding */}
       <div className='flex-1 flex flex-col items-start justify-between p-6 md:p-10
       lg:pl-40'>
         <img src={assets.logo} alt="" className='h-12 object-contain'/>
