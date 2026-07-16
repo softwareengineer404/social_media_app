ecc53dbaadc8efdf8e1746475efdb93308520c29
tesfay <tesfayhailemariam29@gmail.com>
2026-07-16 16:58:55 +0300

    add the missing syntax

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,7 @@ import {SignIn } from '@clerk/clerk-react'
 const Login = () => {
   return (
     <div className='min-h-screen flex flex-col md:flex-row'>
-      {/* background Image */}
+      {/* Background Image */}
       <img src={assets.bgImage} alt="" className='absolute top-0 left-0 -z-1 w-full
       h-full object-cover'/>
       {/* Left Side : Branding */}
