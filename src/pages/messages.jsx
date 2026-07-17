ae997dbfbafdd88dfa3482e77a3e17133e067ebf
tesfay <tesfayhailemariam29@gmail.com>
2026-07-17 18:08:10 +0300

    check it if there is any error

--- src/pages/messages.jsx
+++ src/pages/messages.jsx
@@ -7,7 +7,7 @@ const messages = () => {
   return (
     <div className='min-h-screen relative bg-slate-50'>
       <div className='max-w-6xl mx-auto p-6'>
-        {/* tiltle */}
+        {/* Tiltle */}
         <div className='mb-8'>
           <h1 className='text-3xl font-bold text-slate-900 mb-2'>Messages</h1>
           <p className='text-slate-600'>Talk To Your Friends and Family</p>
