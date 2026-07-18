2909a4c852f2e3a3f970929275c568d2e9ddcee5
tesfay <tesfayhailemariam29@gmail.com>
2026-07-18 17:44:22 +0300

    add the created date for the post

--- src/pages/ChatBox.jsx
+++ src/pages/ChatBox.jsx
@@ -17,7 +17,6 @@ const ChatBox = () => {
     <div className='flex flex-col h-screen'>
       <div className='flex items-center gap-2 p-2 md:px-10 xl:pl-42
       bg-gradient-to-r from-indigo-50 to-purple-50 border-b border-gray-300'>
-
         <img src={user.profile_picture} alt="" className="size-8 rounded-full"/>
         <div>
           <p className="font-medium">{user.full_name}</p>
