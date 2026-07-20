fddab95d7d1755cf801e14ca6c141c6623e38308
tesfay <tesfayhailemariam29@gmail.com>
2026-07-20 15:27:19 +0300

    capitalize the comment

--- src/pages/messages.jsx
+++ src/pages/messages.jsx
@@ -25,7 +25,7 @@ const messages = () => {
                 className='rounded-full size-12 mx-auto'
               />
               <div className='flex-1'>
-                {/* Content goes here */}
+                {/* Content Goes Here */}
                 <p className='font-medium text-slate-700'>{user.full_name}</p>
                 <p clsssName='text-slate-500'>@{user.username}</p>
                 <p className='text-sm text-gray-600'>{user.bio}</p>
